707cc3ad075cb644cd8be4ef696c8076c6dd7173
Harry Lu <harryzlu14@gmail.com>
2020-06-15 00:17:16 -0400

    added favicons, modified metadata, added placeholder view

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <h1>coming soon binch</h1>
+    <img src="/lily.png" alt="Lily"></img>
+  </div>
+);
+
+export default App;
