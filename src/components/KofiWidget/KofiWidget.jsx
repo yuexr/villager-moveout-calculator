d88e5f22d518782d1f6d9da3f17aaf10a0da2365
Harry Lu <harryzlu14@gmail.com>
2020-06-22 22:47:34 -0400

    add slider marks

--- src/components/KofiWidget/KofiWidget.jsx
+++ src/components/KofiWidget/KofiWidget.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './KofiWidget.scss';
 
 const KofiWidget = () => (
-  <a title="Support me on ko-fi.com" class="KofiWidget"
+  <a title="Support me on ko-fi.com" className="KofiWidget"
     href="https://ko-fi.com/W7W21Q1LX" target="_blank" rel="noopener noreferrer">
     <img src="https://storage.ko-fi.com/cdn/cup-border.png" />
     Support Me on Ko-fi
--- src/components/ResidentFriendshipField/ResidentFriendshipField.jsx
+++ src/components/ResidentFriendshipField/ResidentFriendshipField.jsx
@@ -7,10 +7,18 @@ import './ResidentFriendshipField.scss';
 
 const ResidentFriendshipField = ({ residentName, friendshipLevel, onChange }) => {
   const getThumbComponent = (props) => (
-    <span {...props}>
-      <Icon path={mdiHeart} size="24px" />
-    </span>
+    <Icon {...props} path={mdiHeart} size="24px" />
   )
+
+  const marks = [
+    { value: 0, label: "?" },
+    { value: 1, label: "1" },
+    { value: 2, label: "2" },
+    { value: 3, label: "3" },
+    { value: 4, label: "4" },
+    { value: 5, label: "5" },
+    { value: 6, label: "6" }
+  ]
 
   return (
     <div className="ResidentFriendshipField">
@@ -20,6 +28,7 @@ const ResidentFriendshipField = ({ residentName, friendshipLevel, onChange }) =>
       </div>
       <div className="ResidentFriendshipField__slider">
         <Slider min={0} max={6} value={friendshipLevel} onChange={onChange}
+          marks={marks}
           ThumbComponent={getThumbComponent} />
       </div>
     </div>
